e7d62af73d1918916bf8e3ac5b2e032ae13c0811
Mega-Barrel <saurabhjoshi7869@gmail.com>
2022-01-04 11:41:40 +0530

    dashboard app

--- dashboard/spotify_main_app.py
+++ dashboard/spotify_main_app.py
@@ -1,1 +1,28 @@
 # Main dashboard screen
+
+# importing libraries
+import dash
+from dash import dcc
+from dash import html
+from dash.dependencies import Input, Output, State
+
+# dash app
+app = dash.Dash(__name__)
+
+# Dash layout
+app.layout = html.Div(
+    children=[
+        html.H1(
+            children='Spotify Analysis'
+        ),
+
+        html.Div(
+            children='''
+            Dashboard for songs listening pattern.
+            '''
+        ),
+    ]
+)
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
